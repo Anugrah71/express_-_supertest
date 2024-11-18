ff99ae2328ee68c135f3b9b9de344db3c8e110f9
ANUGRAH K <162654910+Anugrah71@users.noreply.github.com>
2024-11-18 08:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
-const app = require('./app');
+/*const app = require('./app');
 app.listen(10000, () => {
   console.log("Server is running on port 3000");
 });
+*/
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
